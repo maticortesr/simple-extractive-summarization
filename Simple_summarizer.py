7608326c684296c9e5c488a51093016d7fcbcf28
maticortesr <matias.cortes.10@gmail.com>
2020-01-16 19:26:06 -0500

    validations and template

--- Simple_summarizer.py
+++ Simple_summarizer.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 class InputForm(Form):
     ratio = FloatField(
-        label='Ratio (ie: 0.3)', default=0.3,
-        validators=[validators.InputRequired()])
+        label='Ratio (0.1-0.99)', default=0.3,
+        validators=[validators.InputRequired(), validators.NumberRange(0.1,0.99,'Ratio must be between 0.1 and 0.99')])
     text = TextAreaField(
         label='Text', default='',
-        validators=[validators.InputRequired()], )
+        validators=[validators.InputRequired(),validators.Length(100, message="Text must be at least 100 characters")]
+    )
 
 @app.route('/', methods=['GET', 'POST'])
 
@@ -22,15 +23,18 @@ def main():
     if request.method == 'POST' and form.validate():       
         ratio = form.ratio.data
         text = form.text.data
+        print('ratio used: ',ratio)
         print('predicting...')
         prediction = predict(text, ratio)
         print('prediction done')
         print(prediction)
-        #return render_template('summary_app_output.html', form=form, prediction=prediction)
-        return render_template('summary_app_index.html', form=form,prediction=prediction, text=text)
+        return render_template('summary_app_output.html', form=form,prediction=prediction, text=text)
+    else:
+        return render_template('summary_app_index.html', form=form)
+
 
 def predict(text, ratio):
     return summarize(text, ratio)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
